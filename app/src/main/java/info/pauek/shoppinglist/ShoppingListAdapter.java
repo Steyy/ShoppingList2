af7312ed9eb5a3bf46711dcb4696fb05a346e0ee
Steyy <javi_areva@hotmail.com>
2017-10-25 18:59:42 +0200

    Cada vez que añado un Item me mueve la lista hacia arriba

--- app/src/main/java/info/pauek/shoppinglist/ShoppingListAdapter.java
+++ app/src/main/java/info/pauek/shoppinglist/ShoppingListAdapter.java
@@ -17,7 +17,7 @@ import java.util.zip.Inflater;
  * Created by Javi on 23/10/2017.
  */
 
-public class ShoppingListAdapter extends ArrayAdapter <String> {
+public class ShoppingListAdapter extends ArrayAdapter <ShoppingItem> {
 //Constructor que llama a constructor de la clase base (ArrayAdpater)
     public ShoppingListAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List objects) {
         super(context, resource, objects);
@@ -33,8 +33,8 @@ public class ShoppingListAdapter extends ArrayAdapter <String> {
             result = inflater.inflate(R.layout.shopping_item, null);
         }
         CheckBox shopping_item = (CheckBox) result.findViewById(R.id.shopping_item); //Busco el CheckBox dentro del item de la lista que he encontrado
-        String item_text = getItem(position);
-        shopping_item.setText(item_text);
+        ShoppingItem item = getItem(position);
+        shopping_item.setText(item.getText());
         return result;
     }
 }
